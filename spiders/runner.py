e065e34aec2be4ed3a98a4adf8f20d31eccb6ec7
StanislavH <HaukkaStas@gmail.com>
2021-09-27 13:19:46 +0500

    Update runner.py

--- spiders/runner.py
+++ spiders/runner.py
@@ -1,13 +1,12 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 
-from leroymerlinparser.spiders.leroymerlin import LeroymerlinSpider
-from leroymerlinparser import settings
+from Leroy.spiders.leroy import LeroySpider
+from Leroy import settings
 
 if __name__ == '__main__':
 	crawler_settings = Settings()
 	crawler_settings.setmodule(settings)
 	process = CrawlerProcess(settings=crawler_settings)
-	process.crawl(LeroymerlinSpider)
+	process.crawl(LeroySpider)
 	process.start()
-
