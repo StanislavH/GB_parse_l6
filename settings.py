b7e33d49902427a237935e174bbd45c9f638c1c4
StanislavH <HaukkaStas@gmail.com>
2021-09-27 13:21:57 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,13 +1,19 @@
+#BOT_NAME = 'books_images'
+BOT_NAME = 'leroy_images'
+
+SPIDER_MODULES = ['Leroy.spiders']
+NEWSPIDER_MODULE = 'Leroy.spiders'
+
+
 ROBOTSTXT_OBEY = False
-LOG_ENABLED = True
-LOG_LEVEL = 'DEBUG'
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
-CONCURRENT_REQUESTS = 16
+
+
+ITEM_PIPELINES = {'Leroy.pipelines.LeroyPipeline': 1,
+                  #'leroymerlinparser.pipelines.DataBasePipeline': 300,
+                  }
+
+FILES_STORE = r'downloaded'
+
+
 DOWNLOAD_DELAY = 3
-CONCURRENT_REQUESTS_PER_DOMAIN = 8
-CONCURRENT_REQUESTS_PER_IP = 8
-IMAGES_STORE = 'images'
-ITEM_PIPELINES = {
-    'leroymerlinparser.pipelines.DataBasePipeline': 300,
-    'leroymerlinparser.pipelines.AvitoPhotosPipeline': 200,
-}
+
