d3884df030e477a3031813d33094455650667e56
StanislavH <HaukkaStas@gmail.com>
2021-09-27 13:22:12 +0500

    Update pipelines.py

--- pipelines.py
+++ pipelines.py
@@ -1,29 +1,28 @@
-import scrapy
-from scrapy.pipelines.images import ImagesPipeline
-from pymongo import MongoClient
+from scrapy.pipelines.files import FilesPipeline
+from scrapy import Request
+from os.path import splitext
 
 
 class DataBasePipeline(object):
     def __init__(self):
-        client = MongoClient('localhost', 27017)
-        self.mongo_base = client.avito_photo
+        # client = MongoClient('localhost', 27017)
+        # self.mongo_base = client.avito_photo
+        pass
 
     def process_item(self, item, spider):
-        collection = self.mongo_base[spider.name]
-        collection.insert_one(item)
-        return item
+        # collection = self.mongo_base[spider.name]
+        # collection.insert_one(item)
+        # return item
+        pass
 
 
-class AvitoPhotosPipeline(ImagesPipeline):
+class LeroyPipeline(FilesPipeline):
     def get_media_requests(self, item, info):
-        if item['photos']:
-            for img in item['photos']:
-                try:
-                    yield scrapy.Request(f'http:{img}')
-                except Exception as e:
-                    print(e)
+        return [Request(x, meta={'filename': item.get('file_name')}) for x in item.get(self.files_urls_field, [])]
 
-    def item_completed(self, results, item, info):
-        if results:
-            item['photos'] = [itm[1] for itm in results if itm[0]]
-        return item
+    def file_path(self, request, response=None, info=None):
+        url = request.url
+        media_ext = splitext(url)[1]
+        return f'full\\{request.meta["filename"]}{media_ext}'
+
+
