1d365e5631941ce30e84d54d2ea66945a8834194
StanislavH <HaukkaStas@gmail.com>
2021-09-27 13:20:52 +0500

    Update items.py

--- items.py
+++ items.py
@@ -1,36 +1,14 @@
 import scrapy
-from scrapy.loader.processors import TakeFirst, Join, MapCompose
+from scrapy.loader.processors import TakeFirst
 
 
-class leroymerlinparserItem(scrapy.Item):
-    def filter_price(value):
-        value = ''.join(value).replace(" ", '')
-        return float(value)
-
-    def del_whitespace(value):
-        for i in range(len(value)):
-            value[i] = str(value[i]).replace('  ', '').replace("\n", '')
-        return value
-
-    def sum_params(val1, val2):
-        for i in range(len(val1)):
-            val1[i] = ([val1[i], val2[i]])
-        return val1
-
-    # define the fields for your item here like:
+class LeroyItem(scrapy.Item):
     _id = scrapy.Field()
-    namee = scrapy.Field()
-    photos = scrapy.Field()
-    params1 = scrapy.Field(input_processor=del_whitespace,
-                           output_processor=TakeFirst(), )
-    params2 = scrapy.Field(input_processor=del_whitespace,
-                           output_processor=TakeFirst(), )
-    params = scrapy.Field(#input_processor=sum_params,
-                          #output_processor=TakeFirst(),
-                          )
-    price = scrapy.Field(
-        input_processor=filter_price,
-        output_processor=TakeFirst(),
-    )
-    print(namee, photos, params, price)
-    pass
+    file_urls = scrapy.Field()
+    files = scrapy.Field()
+    file_name = scrapy.Field(output_processor=TakeFirst())
+    url = scrapy.Field()
+    params1 = scrapy.Field()
+    params2 = scrapy.Field()
+    params = scrapy.Field()
+    price = scrapy.Field()
