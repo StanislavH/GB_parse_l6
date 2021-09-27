61ff8df72b4922f2f1f4234ea212b26fac9dcda3
StanislavH <HaukkaStas@gmail.com>
2021-09-27 13:20:05 +0500

    Update leroymerlin.py

--- spiders/leroymerlin.py
+++ spiders/leroymerlin.py
@@ -1,38 +1,38 @@
-import scrapy
-from scrapy.http import HtmlResponse
-from leroymerlinparser.items import leroymerlinparserItem
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 from scrapy.loader import ItemLoader
-import time
+from Leroy.items import LeroyItem
+from urllib.parse import urljoin
+from scrapy.loader.processors import MapCompose, Compose, Join
 
 
-class LeroymerlinSpider(scrapy.Spider):
+class LeroySpider(CrawlSpider):
     name = 'leroymerlin'
-    allowed_domains = ['leroymerlin.ru']
+    start_urls = [f'https://leroymerlin.ru/catalogue/elektrotovary/']
 
-    def __init__(self):
-        self.start_urls = [f'https://leroymerlin.ru/catalogue/elektrotovary/']
+    rules = (
+        Rule(LinkExtractor(
+            restrict_xpaths='//a[@class="bex6mjh_plp s15wh9uj_plp l7pdtbg_plp r1yi03lb_plp sj1tk7s_plp"]')),
+        Rule(
+            LinkExtractor(restrict_xpaths='//a[@class="bex6mjh_plp b1f5t594_plp p5y548z_plp pblwt5z_plp nf842wf_plp"]'),
+            callback='parse_item')
+    )
 
-    def parse(self, response: HtmlResponse):
-        ads_links = response.xpath(
-            '//a[@class="bex6mjh_plp b1f5t594_plp p5y548z_plp pblwt5z_plp nf842wf_plp"]/@href').extract()
-        for link in ads_links:
-            time.sleep(3)
-            yield response.follow(link, callback=self.parse_ads)
+    def parse_item(self, response):
+        item_loaded = ItemLoader(item=LeroyItem(), response=response)
+        item_loaded.add_xpath('file_name', '//h1/text()', MapCompose(lambda i: i.replace(':', '')))
+        item_loaded.add_xpath('file_urls', '//*/picture[@slot="pictures"]/source[1]/@srcset',
+                              MapCompose(lambda i: urljoin(response.url, i)))
 
-    def parse_ads(self, response: HtmlResponse):
-        loader = ItemLoader(item=leroymerlinparserItem(), response=response)
-        loader.add_css('namee', 'h1.header-2::text')
-        loader.add_xpath('photos', '//*/picture[@slot="pictures"]/source[1]/@srcset')
-        loader.add_xpath('params1', '//*/uc-pdp-section-vlimited/dl/div[@class="def-list__group"]/dt/text()')
-        loader.add_xpath('params2', '//*/uc-pdp-section-vlimited/dl/div[@class="def-list__group"]/dd/text()')
-        params = []
-        params1 = loader.get_collected_values("params1")
-        params2 = loader.get_collected_values("params2")
-        for i in range(len(params1)):
-            params.append([params1[i], params2[i]])
-        #loader.add_value('params', loader.get_collected_values('params1'), loader.get_collected_values('params2'))
-        loader.add_value('params', params)
-        loader.add_xpath('price', '//span[@slot="price"][1]/text()')
-        print(loader.get_collected_values('namee'), loader.get_collected_values('params'),
-              loader.get_collected_values('photos'), loader.get_collected_values('price'))
-        yield loader.load_item()
+        item_loaded.add_value('file_name', '1.jpg')
+        item_loaded.add_xpath('params1', '//*/uc-pdp-section-vlimited/dl/div[@class="def-list__group"]/dt/text()')
+        item_loaded.add_xpath('params2', '//*/uc-pdp-section-vlimited/dl/div[@class="def-list__group"]/dd/text()',
+                              MapCompose(lambda i: i.replace('\n', ''), lambda i: i.replace('  ', '')))
+        item_loaded.add_value('params', [
+            [item_loaded.get_collected_values("params1")[i], item_loaded.get_collected_values("params2")[i]]
+            for i in range(len(item_loaded.get_collected_values("params1")))
+        ])
+        item_loaded.add_value('url', response.url)
+        item_loaded.add_xpath('price', '//span[@slot="price"][1]/text()', MapCompose(Join(), lambda i: i.replace(' ', ''), lambda i: float(i)))
+
+        return item_loaded.load_item()
